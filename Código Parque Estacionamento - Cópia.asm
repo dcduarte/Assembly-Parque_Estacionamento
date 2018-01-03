72aa6f7d793bf7ebe47c7f47c6003a7304c082ad
franciscocosta13 <34520006+franciscocosta13@users.noreply.github.com>
2018-01-03 19:29:28 +0000

    LPL

--- Código Parque Estacionamento - Cópia.asm
+++ Código Parque Estacionamento - Cópia.asm
@@ -3,20 +3,20 @@
                		MOV   R3, 0                      
                		MOV   R4, 0                      
                		MOV   R5, 0                      
-               		MOV   R6, 0                      
+               		MOV   R6, 0 
+			MOV   R7, 0
+			MOV   R8, 0
+			MOV   R9, 0                     
                		JMP   SENSORCARRO
                
-SENSORCARRO: 		MOV   R8, 0
-              		ADD   R1, 1                       
+SENSORCARRO: 		ADD   R1, 1                       
                		ADD   R8, 1
                		JMP   CARROENTRA
 
 CARROENTRA:   		ADD   R2, R1                      
                		ADD   R3, R2
-               		ADD   R8, 0
+               		MOV   R8, 0
+			JMP   CARROSAI
   
 CARROSAI:      		SUB   R2, 1
-               		JZ    SENSORCARRO
-               		ADD   R7, 1
-               		ADD   R9, 1
                		JMP   SENSORCARRO
